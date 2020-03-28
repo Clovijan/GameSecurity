8e27c7fefecebbff4c97b58ad4e243e0abbbee1c
Clovijan <clovijan@gmail.com>
2020-03-28 20:15:58 -0300

    Mudança de fase criada close #4

--- Assests/SCRIPTS/Fase_1.gd
+++ Assests/SCRIPTS/Fase_1.gd
@@ -1,14 +1,14 @@
 extends Control
 
 func _ready():
-	if Game.person == 1:
-		var cena = preload("res://Cenas/PlayeGirll.tscn")
-		var node = cena.instance()
-		add_child(node)
-	elif Game.person == 2:
-		var cena = preload("res://Cenas/PlayerBoy.tscn")
-		var node = cena.instance()
-		add_child(node)
+#	if Game.person == 1:
+#		var cena = preload("res://Cenas/PlayeGirll.tscn")
+#		var node = cena.instance()
+#		add_child(node)
+#	elif Game.person == 2:
+#		var cena = preload("res://Cenas/PlayerBoy.tscn")
+#		var node = cena.instance()
+#		add_child(node)
 	pass
 
  
